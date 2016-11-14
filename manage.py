9399e177bd8ccad9d14cf0cf3b9643ebe695d207
Sai628 <sai100628@gmail.com>
2016-11-14 15:11:10 +0800

    add "Flask-WTF" & sample

--- manage.py
+++ manage.py
@@ -1,22 +1,55 @@
 # coding=utf-8
 
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
 from flask import Flask
 from flask import render_template
+from flask import session
+from flask import url_for
+from flask import redirect
+from flask import flash
+
 from flask_bootstrap import Bootstrap
 from flask_script import Manager
 from flask_moment import Moment
+from flask_wtf import Form
+
+from wtforms import StringField
+from wtforms import SubmitField
+from wtforms.validators import Required
+
 from datetime import datetime
 
 
+class NameForm(Form):
+    name = StringField('你的名字?', validators=[Required()])
+    submit = SubmitField('提交')
+
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret_test'
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', current_time=datetime.utcnow())
+    name_form = NameForm()
+
+    if name_form.validate_on_submit():
+        new_name = name_form.name.data
+        old_name = session.get('name')
+        if old_name is None or old_name == new_name:
+            session['name'] = new_name
+            name_form.name.data = ''
+        else:
+            flash('名字输入错误!')
+        return redirect(url_for('index'))
+
+    return render_template('index.html', form=name_form, name=session.get('name'), current_time=datetime.utcnow())
 
 
 @app.route('/user/<name>')

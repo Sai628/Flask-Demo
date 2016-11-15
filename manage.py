725b91e693758ff44ea21b67146af9acef19781e
Sai628 <sai100628@gmail.com>
2016-11-15 15:27:11 +0800

    add "Flask-SQLAlchemy" sample

--- manage.py
+++ manage.py
@@ -15,12 +15,28 @@ from flask_bootstrap import Bootstrap
 from flask_script import Manager
 from flask_moment import Moment
 from flask_wtf import Form
+from flask_sqlalchemy import SQLAlchemy
 
 from wtforms import StringField
 from wtforms import SubmitField
 from wtforms.validators import Required
 
+import os
 from datetime import datetime
+
+
+baseDir = os.path.abspath(os.path.dirname(__file__))
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret_test'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////' + os.path.join(baseDir, 'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+db = SQLAlchemy(app)
+manager = Manager(app)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 
 class NameForm(Form):
@@ -28,17 +44,29 @@ class NameForm(Form):
     submit = SubmitField('提交')
 
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret_test'
-manager = Manager(app)
-bootstrap = Bootstrap(app)
-moment = Moment(app)
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role')
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     name_form = NameForm()
-
     if name_form.validate_on_submit():
         new_name = name_form.name.data
         old_name = session.get('name')
@@ -48,7 +76,6 @@ def index():
         else:
             flash('名字输入错误!')
         return redirect(url_for('index'))
-
     return render_template('index.html', form=name_form, name=session.get('name'), current_time=datetime.utcnow())
 
 
@@ -60,6 +87,20 @@ def user(name):
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
+
+
+db.create_all()
+role_admin = Role(name='Admin')
+user_tom = User(username='tom', role=role_admin)
+user_jim = User(username='jim', role=role_admin)
+user_tim = User(username='tim', role=role_admin)
+user_sam = User(username='sam', role=role_admin)
+db.session.add(role_admin)
+db.session.add(user_tom)
+db.session.add(user_jim)
+db.session.commit()
+
+print User.query.all()
 
 
 if __name__ == '__main__':
